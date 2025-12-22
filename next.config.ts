44538d9f4018d126a19d566d8ff187493a9aefa7
Wiz Dev Tech <Teriroland15@gmail.com>
2025-12-22 18:20:18 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    runtime: 'edge',  // <-- This enables Edge Runtime globally
+  },
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
