7f77e8ce60620ddc39d6a7c837f5ef7fa88855c7
Wiz Dev Tech <Teriroland15@gmail.com>
2025-12-22 17:12:45 -0500

    .

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    runtime: "edge",
-  },
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
